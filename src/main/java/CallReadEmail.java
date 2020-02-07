e468664bc889589c18024d62bc6ed5c6dcdc385e
Rodrigo Ramirez <rramirez@seiza-ti.cl>
2020-02-07 16:13:02 -0300

    se fusionan puntos de entrada

--- src/main/java/CallReadEmail.java
+++ src/main/java/CallReadEmail.java
@@ -1,17 +1,23 @@
 
-
-public class ReadEmailImap {
-
+public class CallReadEmail {
 
     public static void main(String[] args) {
 
+        /* IMAP
         String host = "imap.gmail.com";
         String mailStoreType = "imap";
         String username = "******@gmail.com";
         String password = "******";
+        */
+
+        /* POP3*/
+        String host = "pop.gmail.com";
+        String mailStoreType = "pop3s";
+        String username = "******@gmail.com";
+        String password = "******";
+
 
         String saveDirectory = "D:/Attachment";
-
         ReadEmail.check(host, mailStoreType, username, password, saveDirectory);
 
     }
